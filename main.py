428a4a2840233d5eb0475cc93e98cb1258b1775f
Азамат <xismatullin2003@inbox.ru>
2023-03-15 19:34:07 +0300

    MY SECONG COMMIT

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def main(num):
+    print(f'Hi, {num}')
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    main('Azamat')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
