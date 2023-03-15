c1549fab3de33d36ce09cab5ab9dc7804dd03b63
Азамат <xismatullin2003@inbox.ru>
2023-03-15 20:28:40 +0300

    SIX

--- minus.py
+++ minus.py
@@ -2,3 +2,4 @@ def minus(num1, num2):
     total = num1 - num2
 
     return total
+print('fd')
--- plus.py
+++ plus.py
@@ -1,3 +1,5 @@
 def plus(*args):
     total = [num for num in args]
     return sum(total)
+
+print('fddfgit ')
